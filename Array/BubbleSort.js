3cc06d36c3e16e30fcda178d97f888132d3fa9d5
rashmi-shree <rashmi.s0297@gmail.com>
2026-01-21 12:08:09 +0530

    bubble sort optimized

--- Array/BubbleSort.js
+++ Array/BubbleSort.js
@@ -1,10 +1,16 @@
 var arr = [7, 12, 9, 11, 3]
 
 for (let i = 0 ; i<arr.length; i++){
+    let swapped = false 
     for (let j = 0 ; j<arr.length -i -1; j++){
         if(arr[j]> arr[j+1]){
             [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            swapped = true
         }
+    }
+
+    if(!swapped){
+        break;
     }
 }
 
