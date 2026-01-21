28a93d979001c32762b51115537c7d245e422a93
rashmi-shree <rashmi.s0297@gmail.com>
2026-01-21 12:29:14 +0530

    selection sort

---
+++ Array/SelectionSort.js
@@ -0,0 +1,16 @@
+let arr = [ 7, 12, 9, 11, 3]
+
+for (let i =0 ; i<arr.length; i++){
+    minIndex = i
+
+    for(j = i+1; j<arr.length; j++){
+        if(arr[j]<arr[minIndex]){
+            minIndex = j
+        }
+    }
+
+    if(minIndex !== i){
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+}
+console.log(arr)
