f2b410c70c76764315f4f3368806c0b391e0acaa
rashmi-shree <rashmi.s0297@gmail.com>
2026-01-22 23:41:41 +0530

    insertion sort

---
+++ Array/insertionSort.js
@@ -0,0 +1,18 @@
+var arr = [5,3,4,1]
+
+
+function isort(){
+    for (let i = 1; i<arr.length ; i++){
+        let current = arr[i]
+        let j = i-1
+
+        while (j>=0 && arr[j]>current){
+            arr[j+1] = arr[j]
+            j--
+        }
+        arr[j+1]=current
+    }
+    return arr
+}
+
+console.log(isort(arr))
