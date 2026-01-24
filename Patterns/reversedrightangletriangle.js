e80cd410b7cd309ea80e2b0e58a01dbb4a03e188
rashmi-shree <rashmi.s0297@gmail.com>
2026-01-24 23:45:48 +0530

    patter

---
+++ Patterns/reversedrightangletriangle.js
@@ -0,0 +1,15 @@
+// * * * * *
+// * * * * 
+// * * * 
+// * *
+// *
+let output = ""
+for (let i = 1 ; i<= 5;i++){
+    for (let j = 0 ; j<5-i+1; j++){
+        output += "*"
+    }
+    output += "\n"
+}
+
+console.log(output);
+
