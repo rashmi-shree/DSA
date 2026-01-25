d54492e61179ef305beb2a380399ab2b657ecde6
rashmi-shree <rashmi.s0297@gmail.com>
2026-01-25 09:15:23 +0530

    basic math

--- BasicMath/printalldivisors.js
+++ BasicMath/printalldivisors.js
@@ -1,8 +1,19 @@
 n = 36 
 
-for(let i =0 ; i<n ; i++){
+// for(let i =0 ; i<n ; i++){
+//     if(n%i == 0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+for(let i = 0 ;i<Math.sqrt(n); i++){
     if(n%i == 0){
         console.log(i);
+        if(n/i != i){
+            console.log(n/i);
+        }
         
     }
 }
