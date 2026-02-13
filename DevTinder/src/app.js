cd683592d3df7eb54116a7eb6b8b85ff9c132249
rashmi-shree <rashmi.s0297@gmail.com>
2026-02-13 09:29:35 +0530

    backend code also added

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -2,13 +2,29 @@ const express = require ("express")
 
 const app = express()
 
+app.use("/test", (req, res)=>{
+    res.send("hello")
+})
+app.get("/user1",(req,res,next)=>{
+    next()
+},
+(req,res)=>{
+    res.send("2nd response")
+})
+app.get("/user/:userid", (req, res)=>{
+    // console.log("query:", req.query);
+    console.log("query:", req.params);
+    
+    res.send({firstname:"rashmi"})
+})
+app.post("/user", (req,res)=>{
+    res.send("data has been successfully saved in the db")
+})
 app.use((req,res)=>{
     res.send("hello from the server")
     
 })
-app.use("/test", (req, res)=>{
-    res.send("hello")
-})
+
 app.listen(3000, ()=>{
     console.log("server is listening on port 3000");
     
