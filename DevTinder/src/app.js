65512929b836bb41897d59941eb56317874b5052
rashmi-shree <rashmi.s0297@gmail.com>
2026-03-10 17:22:17 +0530

    data sanitization

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -15,16 +15,63 @@ app.post("/signup", async(req, res)=>{
     }catch(err){
         res.status(400).send("error while saving"+ err.message)
     }
-    // const userObj = {
-    //     firstName:"Ms",
-    //     lastName:"asf",
-    //     emailId:"asd@adsf.asd",
-    //     password:"asffds"
-    // }
-    // const user = new User(userObj)
+})
 
-    // await user.save();
-    // res.send("user added successfully")
+// get user by email
+app.get("/user", async (req, res)=>{
+    try{
+        const user = await User.find({emailId:req.body.emailId})
+        res.send(user)
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+})
+
+//get all users from the database 
+app.get("/feed", async(req, res)=>{
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch(err){
+        res.status(400).send("something went wrong", err.message)
+    }
+})
+//delete user
+app.delete("/user", async(req, res)=>{
+    try{
+        // const user = await User.findByIdAndDelete(req.body.userId)
+
+        const user = await User.findOneAndDelete({ _id: req.body.userId })
+        res.send({
+            message: "user deleted successfully",
+            user
+            })
+    }catch(err){
+        res.status(400).send("something went wrong", err.message)
+    }
+})
+
+//update user
+app.patch('/user/:userId', async(req, res)=>{
+    const userId = req.params?.userId;
+    const data = req.body
+    
+    try{
+        const ALLOWED_UPDATES = ['photoUrl', 'about', 'gender', 'age']
+    const isUpdateAllowed = Object.keys(data).every((k)=>
+    ALLOWED_UPDATES.includes(k))
+    if(!isUpdateAllowed){
+        throw new Error ("update not allowed")
+    }
+    if(data.skills > 10){
+        throw new Error("skills cannot be more than 10 ")
+    }
+        await User.findByIdAndUpdate({_id:data}, data)
+        res.send("user updated success")
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
 })
 
 connectDb()
--- DevTinder/src/models/user.js
+++ DevTinder/src/models/user.js
@@ -2,24 +2,41 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
         type:String
     },
     gender:{
         type:String
+    },
+    photoUrl:{
+        type:String,
+        default:""
+    },
+    about:{
+        type:String,
+        default:"this is default about"
+    },
+    skills:{
+        type:[String]
     }
-})
+}, { timestamps: true })
 
 const userModel = mongoose.model("user", userSchema)
 module.exports = userModel
